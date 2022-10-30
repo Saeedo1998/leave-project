ce9966a95ee28c638e607bab3393a2701cb6e490
Saeed Badghaish <saeedo_98@hotmail.com>
2022-10-30 12:48:29 +0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeaveRequestComponent } from './Employee/leave-request/leave-request.component';
+import { AddEmployeeComponent } from './Manager/add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './Manager/view-employee/view-employee.component';
+import { ViewRequestComponent } from './Manager/view-request/view-request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeaveRequestComponent,
+    AddEmployeeComponent,
+    ViewEmployeeComponent,
+    ViewRequestComponent
   ],
   imports: [
     BrowserModule,
